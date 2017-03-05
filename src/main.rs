8569bc95b63fe288948afe3ff991fbe9eb06a2c3
Andrew O'Neill <foolusion@gmail.com>
2017-03-04 18:33:19 -0800

    have a somewhat working version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate sha1;
+extern crate byteorder;
+
+use byteorder::ReadBytesExt;
 
 fn main() {
     println!("Hello, world!");
@@ -7,11 +10,17 @@ fn main() {
         namespace: "namespace",
         params: vec![Param {
                          name: "p1",
-                         choices: vec!["1", "2"],
-                         weights: vec![],
+                         choices: vec![Choice {
+                                           name: "1",
+                                           weight: 0.0,
+                                       },
+                                       Choice {
+                                           name: "2",
+                                           weight: 0.0,
+                                       }],
                      }],
     };
-    eval_test(&e, "123");
+    println!("{:?}", eval_test(&e, "13").unwrap());
 }
 
 struct Experiment<'a> {
@@ -22,17 +31,52 @@ struct Experiment<'a> {
 
 struct Param<'a> {
     name: &'a str,
-    choices: Vec<&'a str>,
-    weights: Vec<f64>,
+    choices: Vec<Choice<'a>>,
 }
 
-fn eval_test(exp: &Experiment, userid: &str) {
+struct Choice<'a> {
+    name: &'a str,
+    weight: f64,
+}
+
+#[derive(Debug)]
+struct Experience<'a> {
+    name: &'a str,
+    namespace: &'a str,
+    params: Vec<ParamExperience<'a>>,
+}
+
+#[derive(Debug)]
+struct ParamExperience<'a> {
+    name: &'a str,
+    choice: &'a str,
+}
+
+fn eval_test<'a>(exp: &'a Experiment, userid: &str) -> Result<Experience<'a>, std::io::Error> {
+    let mut params: Vec<ParamExperience> = Vec::new();
     for elem in exp.params.iter() {
         let mut m = sha1::Sha1::new();
         m.update(exp.namespace.as_bytes());
+        m.update(b":");
         m.update(exp.name.as_bytes());
+        m.update(b":");
         m.update(elem.name.as_bytes());
+        m.update(b":");
         m.update(userid.as_bytes());
-        println!("{}", m.digest().to_string())
+        let mut cur = std::io::Cursor::new(m.digest().bytes());
+        let hash: u64 = match cur.read_u64::<byteorder::BigEndian>() {
+            Ok(i) => i,
+            Err(err) => return Err(err),
+        };
+
+        params.push(ParamExperience {
+            name: elem.name,
+            choice: elem.choices[(hash % (elem.choices.len() as u64)) as usize].name,
+        })
     }
+    Ok(Experience {
+        name: exp.name,
+        namespace: exp.namespace,
+        params: params,
+    })
 }
